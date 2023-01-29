059f953587be533f43d3615fced0cecc16d7d725
moscarellar <moscarellar@gmail.com>
2023-01-29 00:17:48 +0000

    indentation error in setting line 65 corrected

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -62,7 +62,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
- ALLOWED_HOSTS = ['localhost', 'drf-ci.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', 'drf-ci.herokuapp.com']
 
 
 # Application definition
