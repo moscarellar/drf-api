b67adc0500b017367268f45c12be5de60ab96482
moscarellar <moscarellar@gmail.com>
2023-05-21 12:23:18 +0000

    Revert "remove dupliacte cors from installed apps"
    This reverts commit 7559deb5cad136ff1430fcb149c1bb75d9ae737a.

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -81,6 +81,7 @@ CORS_ALLOW_CREDENTIALS = True
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
