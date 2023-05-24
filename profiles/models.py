20e785f904d4cc290f1fcff28a3e1b1bd18081a1
moscarellar <moscarellar@gmail.com>
2023-05-24 17:52:10 +0000

    Revert "modified profile image address"
    This reverts commit d8ac2436f44ffe0c08bee366a066c47ffad83d07.

--- profiles/models.py
+++ profiles/models.py
@@ -10,7 +10,7 @@ class Profile(models.Model):
     name = models.CharField(max_length=255, blank=True)
     content = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to='images/', default='../default_profile_otz1yg'
+        upload_to='images/', default='v1683695518/media/images/default_profile_otz1yg'
     )
 
     class Meta:
